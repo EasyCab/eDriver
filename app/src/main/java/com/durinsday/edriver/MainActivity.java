82715cc0cd2054a7c865006a6e59be3d61346446
EasyCab <42908726+EasyCab@users.noreply.github.com>
2018-09-17 20:42:21 +0500

    Login Maps done...!

--- app/src/main/java/com/durinsday/edriver/MainActivity.java
+++ app/src/main/java/com/durinsday/edriver/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
                 startActivity(intent);
+
                 finish();
                 return;
             }
